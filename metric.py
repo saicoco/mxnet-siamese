36ec38aacc94ed8a2d45163a96df0b8a53634179
saicoco <gjj0816@gmail.com>
2017-04-18 21:26:54 +0800

    siamese_metric

--- metric.py
+++ metric.py
@@ -13,9 +13,6 @@ class siamise_metric(mx.metric.EvalMetric):
         labels = label[0]
         preds_label = preds.asnumpy().ravel()
         labels = labels.asnumpy().ravel()
-        preds_label[preds_label >= 0.5] = 1.0
-        preds_label[preds_label < 0.5] = 0
-        preds_label = 1.0 - preds_label
-        self.sum_metric += (labels==preds_label).sum()
+        self.sum_metric += labels[preds_label < 0.5].sum()
         self.num_inst += len(labels)
 
